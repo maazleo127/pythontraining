dd2aed063cd25ab133e51ff3c02f8161943f7bbc
maazleo127 <sm.maaz127@gmail.com>
2023-02-15 18:39:39 +0530

    handling

---
+++ exceptional handling.py
@@ -0,0 +1,12 @@
+# def res(n):
+#     try:
+#         l=[1,2,3]
+#         print(l[3])
+#         res(n)
+#         return n-3/0
+#     except ZeroDivisionError:
+#         print('zero divison error has accured!you cannot divide 0')
+#     except IndexError:
+#         print('index error has occured')
+
+# res(0)
---
+++ print_statement.py
@@ -0,0 +1,11 @@
+# # my_list = [1, 2, 3, 4, 5]
+# # copy_list = my_list.copy()
+# # print('Old List: ', my_list)
+# # print('New List: ', copy_list)
+
+# name = "Chris"
+# email = "chris123@gmail.com"
+# print(len(name)) #prints 5
+# print(name.upper()) #prints "CHRIS"
+# both=name+' '+email
+# print(both)
