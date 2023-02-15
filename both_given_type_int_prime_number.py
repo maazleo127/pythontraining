e88772152e894a390ff26dd71a6ebd0775187353
maazleo127 <sm.maaz127@gmail.com>
2023-02-15 15:38:39 +0530

    day2

---
+++ both_given_type_int_prime_number.py
@@ -0,0 +1,16 @@
+# a=11
+# b=12
+# print(type(a))
+# print(type(b))
+
+# l_range = int(input("Enter Lower Range: "))
+# u_range = int(input("Enter Upper Range: "))
+# print("Prime numbers between", l_range, "and", u_range, "are:")
+
+# for num in range(l_range, u_range):
+#     if num > 1:
+#       for i in range(2, num):
+#         if (num % i) == 0:
+#             break
+#         else:
+#             print(num)
--- sum _of_n_numbers.py
+++ sum _of_n_numbers.py
@@ -4,3 +4,10 @@ for i in range(1,100):
     sum=sum+i
 
 print(sum)
+
+x=str(input('onlystring'))
+print(type(x))
+y=int(input('onlyint'))
+print(type(y))
+z=float(input('onlyfloat'))
+print(type(z))
