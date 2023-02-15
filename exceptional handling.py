23cd2cb85ed4065cf20e126a95a3e465031786e5
maazleo127 <sm.maaz127@gmail.com>
2023-02-15 19:27:50 +0530

    handling2

--- exceptional handling.py
+++ exceptional handling.py
@@ -1,12 +1,17 @@
-# def res(n):
-#     try:
-#         l=[1,2,3]
-#         print(l[3])
-#         res(n)
-#         return n-3/0
-#     except ZeroDivisionError:
-#         print('zero divison error has accured!you cannot divide 0')
-#     except IndexError:
-#         print('index error has occured')
+# # def res(n):
+# #     try:
+# #         l=[1,2,3]
+# #         print(l[3])
+# #         res(n)
+# #         return n-3/0
+# #     except ZeroDivisionError:
+# #         print('zero divison error has accured!you cannot divide 0')
+# #     except IndexError:
+# #         print('index error has occured')
 
-# res(0)
+# # res(0)
+# #customexceptions
+# def validate(name):
+#     if len(name)<10:
+#         raise ValueError('name to short')
+# validate(joe)
