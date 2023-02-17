e0ac3a770bafb42917934b9f549fd61cf8c58bda
maazleo127 <sm.maaz127@gmail.com>
2023-02-17 11:06:32 +0530

    list

---
+++ list/list_with_ops.py
@@ -0,0 +1,27 @@
+list=[1,2,3,4,5,6,7,8,9,11,12,13,14,15]
+list1=[33,34,35,36,37,38,39,40]
+# # list.append(64)
+# # print(list)
+# # for x in list:
+# #     list1.append(list1)
+# # print(list1)
+# list1.clear()
+# print(list1)
+# x=list.copy()
+# print(x)
+# x=list.count(9)
+# print(x)
+# x=list.index(11)
+# print(x)
+# list.insert(5,'insert')
+# print(list)
+# def myFunc(e):
+#   return e['year']
+# cars = [
+#   {'car': 'Ford', 'year': 2005},
+#   {'car': 'Mitsubishi', 'year': 2000},
+#   {'car': 'BMW', 'year': 2019},
+#   {'car': 'VW', 'year': 2011}
+# ]
+# cars.sort(key=myFunc) 
+# print(cars)
