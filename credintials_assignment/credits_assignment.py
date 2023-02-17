4fa64e51cf892a9831033da571009257151db651
maazleo127 <sm.maaz127@gmail.com>
2023-02-17 17:57:17 +0530

    creditassignment

---
+++ credintials_assignment/credits_assignment.py
@@ -0,0 +1,15 @@
+valid_credentials={'user1':'password1' ,
+'user2':'password2',
+                    'user3':'password3'}
+print(type(valid_credentials))
+print(valid_credentials.keys())
+
+username=input('enterusername: ')
+password=input('enterurpassword:')
+if username in valid_credentials:
+    if password == valid_credentials[username]:
+      print("login") 
+    else:
+        print('notlogin')
+else:
+    print('invalidusername')
