0d58024f61fbd66cfcf5cbc32c8331d02fa1c19c
maazleo127 <sm.maaz127@gmail.com>
2023-02-17 11:39:47 +0530

    dictionary

---
+++ dictionary/dictionary_ops.py
@@ -0,0 +1,66 @@
+# # # # # x = ('key1', 'key2', 'key3')
+# # # # # y = (0,1,2)
+# # # # # 
+# # # # # thisdict = dict.fromkeys(x, y)
+# # # # # 
+# # # # # print(thisdict)
+
+# # # # # x = ('key1', 'key2', 'key3')
+
+# # # # # thisdict = dict.fromkeys(x)
+
+# # # # # print(thisdict)
+# # # # car = {
+# # # #   "brand": "Ford",
+# # # #   "model": "Mustang",
+# # # #   "year": 1964
+# # # # }
+
+# # # # x = car.get("model")
+
+# # # # print(x) 
+# # # car = {
+# # #   "brand": "Ford",
+# # #   "model": "Mustang",
+# # #   "year": 1964
+# # # }
+
+# # # x = car.setdefault("model", "Bronco")
+
+# # # print(x) 
+# # # car = {
+# # #   "brand": "Ford",
+# # #   "model": "Mustang",
+# # #   "year": 1964
+# # # }
+
+# # # x = car.setdefault("color", "white")
+
+# # # print(x)
+# # car = {
+# #   "brand": "Ford",
+# #   "model": "Mustang",
+# #   "year": 1964
+# # }
+
+# # car.update({"color": "White"})
+
+# # print(car) 
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# x = car.values()
+
+# print(x) 
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# 
+# x = car.keys()
+# 
+# print(x) 
