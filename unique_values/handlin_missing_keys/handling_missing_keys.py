266fcc5bf4643ff0335e9bc6bd9e928af13db454
maazleo127 <sm.maaz127@gmail.com>
2023-02-17 15:16:03 +0530

    day4

---
+++ unique_values/handlin_missing_keys/handling_missing_keys.py
@@ -0,0 +1,2 @@
+# x={1:'abc',2:'avdbc',3:'abccac'}
+# print(x.get(6,"404"))
---
+++ unique_values/unique_values.py
@@ -0,0 +1,9 @@
+# my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 1, 'e': 2, 'f': 4}
+
+# unique_values = set(my_dict.values())
+
+# print(unique_values)
+
+# #convert dict into sets it eliminates the duplicate values.as set cant take duplicate values.
+x={1:'abc',2:'avdbc',3:'abccac'}
+print(x.get(6,"404"))
