4c35313c0ea7e73a07071b8049344d6ce2f6c442
maazleo127 <sm.maaz127@gmail.com>
2023-02-20 18:47:21 +0530

    day2

--- fibonaaci_number/fibonaaxi_numberr.py
+++ fibonaaci_number/fibonaaxi_numberr.py
@@ -1,18 +1,31 @@
-def Fibonacci(n):
+# def Fibonacci(n):
 
 	
-	if n < 0:
-		print("Incorrect input")
+# 	if n < 0:
+# 		print("Incorrect input")
 
-	elif n == 0:
-		return 0
+# 	elif n == 0:
+# 		return 0
 
-	# Check if n is 1,2
-	# it will return 1
-	elif n == 1 or n == 2:
-		return 1
+# 	# Check if n is 1,2
+# 	# it will return 1
+# 	elif n == 1 or n == 2:
+# 		return 1
 
-	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
+# 	else:
+# 		return Fibonacci(n-1) + Fibonacci(n-2)
 
-print(Fibonacci(11))
+# print(Fibonacci(11))
+
+# n=int(input('enter a number :'))
+# first=0
+# second=1
+# count = 2
+# print(first)
+# print(second)
+# while count<=n:
+#     result=first+second
+#     print(result)
+#     first=second
+#     second=result
+#     count+=1
---
+++ reduce_map_lambda/lambda.py
@@ -0,0 +1,15 @@
+# lambda-functionwithout name.it can take any number of arguments but returns only one value.
+# program to calculate sum of three number using lambda function.single line function
+# 
+# def sum(a,b,c):
+    # return(a+b+c)
+
+# add=lambda a,b,c:print(a+b+c)
+# add(1,2,3)
+
+# cube=lambda x:x**3
+# print(cube(6))
+
+# result= [lambda x=i: print(x*2) for i in range(1,6)]
+# for val in result:
+#     print(val())
