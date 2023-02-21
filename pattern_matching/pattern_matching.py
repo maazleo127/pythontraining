329d427f6417a9523e4f54c1f7852d970e5f34e6
maazleo127 <sm.maaz127@gmail.com>
2023-02-21 16:39:34 +0530

    matching

---
+++ pattern_matching/pattern_matching.py
@@ -0,0 +1,12 @@
+x=int(input('enter ur number'))
+match x:
+    case 1:
+        print('u voted for congress')
+    case 2:
+        print('u voted gor bjp')
+    case 3:
+        print('ui voted for sena')
+    case 4:
+        print('u voted for haathi')
+    case _:
+        print('nota')
