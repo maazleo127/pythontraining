af15dbfb170d1090c29444b1130c96a77c396395
maazleo127 <sm.maaz127@gmail.com>
2023-02-21 17:39:00 +0530

    cc

---
+++ find_substring_using_rex/find_substring_using_regex.py
@@ -0,0 +1,10 @@
+sentence=('nancy is 90 years old she need 120 toffees')
+# num=[]
+# for word in sentence.split():
+#     if word.isdigit():
+#         num.append(word)
+# print(num)
+
+# import re
+# digit=re.findall('\d+',sentence)
+# print(digit)
