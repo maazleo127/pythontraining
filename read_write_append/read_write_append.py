f39515cfaab3516911c60dc80d4a4df1d7b6bdb1
maazleo127 <sm.maaz127@gmail.com>
2023-02-22 14:38:46 +0530

    mm

---
+++ read_write_append/read_write_append.py
@@ -0,0 +1,10 @@
+# # file_read = open('/home/ts/Documents/GitHub/pythonlearning/read_write_append/hhh.txt')
+# # print(file_read.read())
+# # file_read=open('/home/ts/Documents/GitHub/pythonlearning/read_write_append/hhh.txt','a')
+# # file_read.write('bahot ache')
+# # file_read.close
+# # print(file_read.append())
+# file_read=open('/home/ts/Documents/GitHub/pythonlearning/read_write_append/hhh.txt','a')
+# file_read.write('bahot ache\n')
+# file_read.close
+
