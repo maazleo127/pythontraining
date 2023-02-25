294a8d1fb20e0639961ba6fc8ee77b29fed5d847
maazleo127 <sm.maaz127@gmail.com>
2023-02-25 17:51:36 +0530

    sudhir

---
+++ factorial_using_recursive_function/test.py
@@ -0,0 +1,38 @@
+# # x=66
+# # y=float(x)
+# # print(y)
+# z=[1,2,3,4,5]
+# print(z[])
+
+# l=[1,2,3,4,5,6,7,8,9,10]
+# # z=[]
+# # print(l)
+# # for i in [1,2,3,4,5,6,7,8,9,10]:
+# #     if i % 2 == 0:
+# #         z.append(i)
+# m=[]
+# for i in l:
+# i=0
+# while i<len(l):
+#     if l[i] % 2 == 0:
+#         m.append(l[i])
+#     i+=1
+# print(m)
+# def add(*args):
+#     sum = 0
+#     for i in args:
+#         sum += i
+#     return sum
+# a=add(5,6,5,6,7,8,10)
+# print(a)
+# z = [i for i in [1,2,3,4,5,6,7,8,9,10]]
+# a=["a",1,2,3,"b","c"]
+# b=[]
+# c=[]
+# for i in a:
+#     if type(i) == int:
+#         b.append (i)
+#     else:
+#         c.append(i)
+        
+# print(c)
