c429a3997afff1deaf1b293aa34716e2585fb667
maazleo127 <sm.maaz127@gmail.com>
2023-04-13 09:10:50 +0530

    app

---
+++ DemoProject/mrbean/auction/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JrBeanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jr_bean'
--- DemoProject/mrbean/auction/models.py
+++ DemoProject/mrbean/auction/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Poll(models.Model):
     question = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -25,4 +25,6 @@ class Vote(models.Model):
     voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ("poll", "voted_by") 
+        unique_together = ("poll", "voted_by")
+
+# Create your models here.
